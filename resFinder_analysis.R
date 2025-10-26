76939adc08a562406f71012db6b118699fdb8f6f
Joel <joel1@live.se>
2025-10-26 20:01:54 +0100

    Improved structure, get gene data, calc. closures by AMR class

--- resFinder_analysis.R
+++ resFinder_analysis.R
@@ -1,5 +1,16 @@
+############################################################
+# Title: Metagenomic MapStat Processing
+# Author: Joel Pålsson
+# Date: 26-10-2025
+# Description: Loads .mapstat files, cleans nested data,
+#              extracts taxonomy, and computes basepair totals.
+############################################################
+
+# 1. Setup
+#===========================================================
 library(tidyverse)
 library(tools)
+library(compositions)
 
 taxonomy = c(
   "Domain",
@@ -13,11 +24,16 @@ taxonomy = c(
 #Enter path to files
 path_resFinder = "data/kma_resfinder/"
 path_silva = "data/kma_silva/"
+path_resFinder_genes = "data/resfinder_genes.csv"
 
 #Enter taxonomic level to plot
 tax_level = "Phylum"
 
-clean = function(nested_data) {
+# 2. Function definitions
+#===========================================================
+
+# ---- 3.1 Load .mapstat files ----
+clean = function(nested_data, taxonomy) {
   nested_data <- nested_data %>% 
     mutate(data = map(data, ~ .x %>%                              #Take each tibble in data column and apply functions  
                         separate_wider_delim("refSequence",                            #Separate Template into Taxonomy and Template
@@ -25,14 +41,24 @@ clean = function(nested_data) {
                                              delim = " ",
                                              too_many = "merge") %>% 
                         separate_wider_delim("Taxonomy",                            #Separate Taxonomy into each taxonomic group
-                                             names =taxonomy,
+                                             names = taxonomy,
                                              delim = ";",
                                              too_few = "align_start",
-                                             too_many = "merge") 
-    )
-    )
+                                             too_many = "merge")))
+  
+  #Calculate total basepairs for each sample
+  bp_total <- nested_data %>%
+    unnest() %>% 
+    group_by(sample) %>% 
+    filter(Domain == "Bacteria") %>% 
+    summarize(bpTotal_sum = sum(bpTotal))
+  
+  #Merge total basepairs with corresponding sample
+  nested_data <- nested_data %>% 
+    merge(bp_total)
 }
 
+# ---- 3.2 Clean nested Silva data ----
 load_mapstat = function(path) {
   files <- dir(path = path,               #Get all files in directory
                recursive = TRUE,
@@ -59,23 +85,67 @@ load_mapstat = function(path) {
                                refSequence = `# refSequence`)))
 }
 
-#Load resFinderdata
+# ---- 3.3 Apply closure ----
+closure_table = function(unnested_data) {
+  
+  #Adjust bpTotals with gene lengths and apply closure
+  unnested_data <- unnested_data %>% 
+    group_by(sample) %>% 
+    mutate(closure = clo(bpTotal / geneLength, total = 100)) %>% 
+    group_by(sample, geneClass) %>%
+    summarise(classClosure = sum(closure, na.rm = TRUE)) %>%
+    ungroup() %>%
+    rename(`AR_Class` = geneClass,
+           Sample = sample)
+}
+
+# 4. Data Loading
+# ==========================================================
+
 resFinder_data_nested <- load_mapstat(path_resFinder)
 
-#Load Silva data
 silva_data_nested <- load_mapstat(path_silva) %>% 
-  clean()
+  clean(taxonomy)
 
-#Unnest data
+resFinder_genes <- read_csv(path_resFinder_genes) %>% 
+  rename(geneLength = Length,
+         geneClass = Class)
+
+# 5. Data Wrangling
+# ==========================================================
+
+# ---- 5.1 Merge resFinder data ----
+
+#Add bpTotal to resFinder tibble
+resFinder_data_nested <- resFinder_data_nested %>% 
+  mutate(bpTotal_sum = silva_data_nested$bpTotal_sum)
+
+# ---- 5.2 Unnest Data ----
 resFinder_data_unnested <- resFinder_data_nested %>% 
   unnest()
 
 silva_data_unnested <- silva_data_nested %>% 
   unnest()
 
-bp_total <- silva_data_unnested %>% 
-  group_by(sample) %>% 
-  filter(Domain == "Bacteria") %>% 
-  pull(bpTotal) %>% 
-  sum()
+# ---- 5.3 Merge resFinder data with gene data, and get closures by AMR class ----
+
+#Merge resFinder data with resFinder gene data
+resFinder_data_unnested <- merge(resFinder_data_unnested,
+                                 resFinder_genes)
+#Get closures
+closures = closure_table(resFinder_data_unnested)
+
+#Concept plot
+closures %>%
+  ggplot(aes(x = Sample, y = classClosure, fill = AR_Class)) +
+  geom_bar(stat = "identity") +
+  theme_minimal() +
+  labs(
+    x = "Sample",
+    y = "Class Closure Value",
+    fill = "AR Class",
+    title = "AR Class Distribution per Sample"
+  ) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 
